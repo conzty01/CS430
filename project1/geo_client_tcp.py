fbb26b84838da7ce669b39571558255071d06679
conzty01 <conzty01@luther.edu>
2018-09-07 10:56:22 -0500

    Completed project1
    Created connection between client script and server script. Client
    prompts user to enter a country. Server looks up the capital of
    provided country and returns the result to the client. If client
    enters "BYE", the connection is closed.

--- project1/geo_client_tcp.py
+++ project1/geo_client_tcp.py
@@ -11,8 +11,20 @@ PORT = 4300
 
 def client():
     '''Main client loop'''
-    # TODO: Implement client-side tasks
-    pass
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        data = s.recv(1024)
+        serveName = data.decode().split()[-1]
+        print('Connected to {}'.format(serveName))
+
+        inStr = input("Enter a country or 'BYE' to quit\n")
+        while inStr != "BYE":
+            s.sendall(inStr.encode())
+            data = s.recv(1024)
+            print(data.decode())
+            inStr = input("Enter another country to try again or 'BYE' to quit\n")
+        s.close()
+        print('Connection closed')
 
 def main():
     '''Main function'''
--- project1/geo_server_tcp.py
+++ project1/geo_server_tcp.py
@@ -10,16 +10,41 @@ HOST = 'localhost'
 PORT = 4300
 
 
-def read_file(filename: str) -> dict:
+def read_file(filename: str): # -> dict:
     '''Read world territories and their capitals from the provided file'''
     world = dict()
+    with open(filename,"r") as f:
+        for line in f:
+            sl = line.split("-")
+            world[sl[0].rstrip()] = sl[-1].lstrip()
     return world
 
 
-def server(world: dict) -> None:
+def server(worldDict: dict): # -> None:
     '''Main server loop'''
-    # TODO: Implement server-side tasks
-    pass
+
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(1)
+        print('Listening on {}:{}'.format(HOST,PORT))
+        conn, addr = s.accept()
+
+        with conn:
+            print('Accepted connection from {}'.format(addr[0]))
+            conn.sendall("Hi, I'm GEO101".encode())
+
+            while True:
+                data = conn.recv(1024)
+                dataStr = data.decode()
+
+                print("User Query:".format(dataStr))
+
+                if dataStr in worldDict.keys():
+                    conn.sendall(worldDict[dataStr].encode())
+                else:
+                    conn.sendall("There is no such country".encode())
+
+        print('Disconnected {}'.format(addr))
 
 
 def main():
